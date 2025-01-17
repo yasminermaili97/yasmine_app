1e2a396292d005a899e079bbc4a59d09f04d604a
yasminermaili97 <yasminermaili2020@gmail.com>
2025-01-17 20:33:24 +0100

    a

--- src/js/logros.js
+++ src/js/logros.js
@@ -1,10 +1,42 @@
-function toggleContent(sectionId) {
-    const content = document.getElementById(sectionId);
-  
-   
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("#btnpremios");
+  const textBox = document.querySelector("#premios");
+
+
+  button.addEventListener("click", () => {
+    textBox.classList.toggle("active");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("#btnimpacto");
+  const textBox = document.querySelector("#impacto");
+
+
+  button.addEventListener("click", () => {
+    textBox.classList.toggle("active");
+  });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("#btncifras");
+  const textBox = document.querySelector("#cifras");
+
+
+  button.addEventListener("click", () => {
+    textBox.classList.toggle("active");
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("#btnlegado");
+  const textBox = document.querySelector("#legado");
+
+
+  button.addEventListener("click", () => {
+    textBox.classList.toggle("active");
+  });
+});
